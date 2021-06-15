58da0cfd6a4a88e70a0c8f5d87c9e774eb07791e
josealonso <jose.9642@hotmail.com>
2021-06-15 16:15:58 +0200

    Listado terminado
    Se ha completado el lista y acesso de todoas las conexiones asignadas al usuario activo

--- src/Controller/ListaController.php
+++ src/Controller/ListaController.php
@@ -11,34 +11,40 @@ use App\Repository\ConexionesRepository;
 class ListaController extends AbstractController {
 
     /**
-     * @Route("/inicio/lista/{ruta}", name="lista_archivos")
+     * @Route("/inicio/lista/{conexion}/{ruta}", name="lista_archivos")
      */
-    public function index(httpClient $cliente, string $ruta = "", ConexionesRepository $conerepo): Response {
+    public function index(httpClient $cliente, ConexionesRepository $conerepo, string $ruta = "", string $conexion = ""): Response {
         // rclone rcd --rc-serve --rc-no-auth
-      //  $userlog = $this->getUser()->getUsername();
+       
         $userlog = $this->getUser()->getId();
-        $criteria=['user' => $userlog ];
-        $conexiones=$conerepo->find('2');
-        var_dump($conexiones->getNombre());
-        /*
+      
+
+
         $final = preg_replace('/_/', '/', $ruta);
+        $lista = array();
 
-        $lista = $cliente->lista('jose:', $final);
-
-        $separados = $cliente->separar($lista);
-
-        $carpetas = $separados['carpeta'];
-        $archivos = $separados['archivos'];
+        if (!empty($conexion)) {
+            $criteria = ['nombre' => $conexion];
+            $conexiones_BD = $conerepo->findBy($criteria);
+        } else {
+            $criteria = ['user' => $userlog];
+            $conexiones_BD = $conerepo->findBy($criteria);
+        }
+        foreach ($conexiones_BD as $array) {
+            $archivos_asociados = $cliente->lista($array->getNombre() . ":", $final);
+            $separados = $cliente->separar($archivos_asociados);
+            $carpetas = $separados['carpeta'];
+            $archivos = $separados['archivos'];
+            $lista[$array->getNombre()] = ['carpetas' => $carpetas, 'archivos' => $archivos];
+        }
 
 
         return $this->render('lista/index.html.twig', [
                     'controller_name' => 'ListaController',
-                    'carpetas' => $carpetas,
-                    'archivos' => $archivos
+                    'lista' => $lista
         ]);
-         * 
-         */
-        return new Response();
+
+        // return new Response();
     }
 
 }
